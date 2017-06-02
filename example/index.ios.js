5dd5c9a833e39562129f9b3b18c1269839453277
jonatansalas <jonatansalas@live.com.ar>
2017-06-01 21:39:32 -0300

    Upgrade react-native + react versions and general project structure to match newer react-native apps

--- example/index.ios.js
+++ example/index.ios.js
@@ -1,63 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { AppRegistry, StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight
-} from 'react-native';
+import * as MercadoPago from 'react-native-checkout-mercadopago';
+import env from './app.json';
 
-import { startCheckout } from 'react-native-checkout-mercadopago';
+export default class Example extends React.Component {
+    state = {
+        status: null
+    };
 
-const MERCADOPAGO_TEST_PUBLICKEY = 'TEST-ad365c37-8012-4014-84f5-6c895b3f8e0a';
-const MERCADOPAGO_TEST_PREFERENCEID = '150216849-ceed1ee4-8ab9-4449-869f-f4a8565d386f';
+    handleClick = async () => {
+        try {
+            const payment = await MercadoPago.startCheckout(env['public_key'], env['preference_id']);
 
-export default class example extends Component {
+            this.setState({
+                status: JSON.stringify(payment)
+            });
+        } catch (error) {
+            this.setState({
+                status: error.toString()
+            });
+        }
+    };
 
-  constructor(context) {
-    super(context);
-    this.state = { status: null };
-  }
-
-  async handleClick() {
-
-    try {
-
-      let payment = await startCheckout(MERCADOPAGO_TEST_PUBLICKEY, MERCADOPAGO_TEST_PREFERENCEID);
-
-      this.setState({status: JSON.stringify(payment)});
-
-    } catch (err) {
-
-      this.setState({status: err.toString()})
-
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native MercadoPago Checkout
+                </Text>
+                <Text style={styles.instructions}>
+                    Tap the following button to start the checkout flow
+                </Text>
+                <TouchableHighlight onPress={this.handleClick}>
+                    <Text style={styles.button}>
+                        CHECKOUT PRODUCT FOR $100
+                    </Text>
+                </TouchableHighlight>
+                <Text>
+                    {this.state.status}
+                </Text>
+            </View>
+        );
     }
-
-  }
-
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native MercadoPago Checkout
-        </Text>
-        <Text style={styles.instructions}>
-          Tap the following button to start the checkout flow
-        </Text>
-        <TouchableHighlight onPress={ () => this.handleClick() }>
-          <Text style={styles.button}>CHECKOUT PRODUCT FOR $100</Text>
-        </TouchableHighlight>
-        <Text>{this.state.status}</Text>
-      </View>
-    );
-  }
 }
 
 const styles = StyleSheet.create({
@@ -79,4 +64,4 @@ const styles = StyleSheet.create({
   }
 });
 
-AppRegistry.registerComponent('example', () => example);
+AppRegistry.registerComponent('example', () => Example);
